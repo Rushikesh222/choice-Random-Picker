8597a8f6c3a99f2565811d93adb5910765c5030d
Rushikesh Shirsat <shirsatrushikesh6@gmail.com>
2021-11-14 13:58:15 +0530

    complete

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const textarea = document.getElementById("textarea");
 textarea.focus();
 textarea.addEventListener("keyup", (e) => {
   createTags(e.target.value);
+  if (e.key === "Enter") {
+    setTimeout(() => {
+      e.target.value = "";
+    }, 10);
+    RandomSelect();
+  }
 });
 function createTags(input) {
   const tags = input
@@ -16,4 +22,31 @@ function createTags(input) {
     tagEl.innerHTML = tag;
     tagsEl.appendChild(tagEl);
   });
+}
+function RandomSelect() {
+  const time = 30;
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+    highlighttag(randomTag);
+    setTimeout(() => {
+      unhighlighttag(randomTag);
+    }, 100);
+  }, 100);
+  setTimeout(() => {
+    clearInterval(interval);
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+      highlighttag(randomTag);
+    }, 100);
+  }, time * 100);
+}
+function pickRandomTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+function highlighttag(tag) {
+  tag.classList.add("highlight");
+}
+function unhighlighttag(tag) {
+  tag.classList.remove("highlight");
 }
